0a692585ae58f26ac131d8fb23a912de4585e3fa
Okwanwae1 <okwanwae@gmail.com>
2023-06-01 14:26:55 +0000

    I have modified the category file

--- lib/category.dart
+++ lib/category.dart
@@ -1,3 +1,4 @@
+import 'package:e_learning_app_ui/test_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -48,58 +49,66 @@ class category extends StatelessWidget {
                   padding: const EdgeInsets.all(20),
                   child: Column(
                     children: [
-                      Container(
-                        height: 85,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(60),
-                          boxShadow: [BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 5,
-                            spreadRadius: 1,
-                          ),]
-                        ),
-                        child: Row(
-                          children: [
-                             Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 25, top: 20),
-                                  child: Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Text("Qualitative Aptitude", 
-                                     style: TextStyle(
-                                     color: Colors.black,
-                                     fontSize: 20,
-                                    ),),
-                                  ),
-                                ),
-                                Row(
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.only(left: 23, top: 5),
-                                      child: Align(alignment: Alignment.centerLeft, child: Text("25 Mock Test",style: TextStyle(fontSize: 11),)),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const test_page()));
+                    //code to execute when this button is pressed.
+                        },
+                        child: Container(
+                          height: 85,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(60),
+                            boxShadow: [BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5,
+                              spreadRadius: 1,
+                            ),]
+                          ),
+                          child: Row(
+                            children: [
+                               Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 25, top: 20),
+                                    child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text("Qualitative Aptitude", 
+                                       style: TextStyle(
+                                       color: Colors.black,
+                                       fontSize: 20,
+                                      ),),
                                     ),
-                                    
-                                    const Padding(
-                                      padding: EdgeInsets.only(left: 23, top: 5),
-                                      child: Align(alignment: Alignment.centerLeft, child: Text("05 Last Year Exams",style: TextStyle(fontSize: 11),)),
-                                    ), 
-                                    
-                                  ],
-                                ),
-                                
-                              ],
-                            ),
-                            Spacer(),
-                            IconButton(onPressed: () {},
-                             icon: const Icon(
-                            Icons.arrow_forward_sharp,
-                            color: Colors.black,
-                            size: 25,
-                          ),),
-                          ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Padding(
+                                        padding: EdgeInsets.only(left: 23, top: 5),
+                                        child: Align(alignment: Alignment.centerLeft, child: Text("25 Mock Test",style: TextStyle(fontSize: 11),)),
+                                      ),
+                                      
+                                      const Padding(
+                                        padding: EdgeInsets.only(left: 23, top: 5),
+                                        child: Align(alignment: Alignment.centerLeft, child: Text("05 Last Year Exams",style: TextStyle(fontSize: 11),)),
+                                      ), 
+                                      
+                                    ],
+                                  ),
+                                  
+                                ],
+                              ),
+                              Spacer(),
+                              IconButton(onPressed: () {},
+                               icon: const Icon(
+                              Icons.arrow_forward_sharp,
+                              color: Colors.black,
+                              size: 25,
+                            ),),
+                            ],
+                          ),
                         ),
                       ),
             
